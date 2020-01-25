fed1ecd0ce07350b37e75fa7661a819792badc1b
Metaphysiker <s.raess@me.com>
2020-01-25 22:35:47 +0100

    adding french translation

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,22 @@ Rails.application.routes.draw do
     root 'static_pages#biel'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    localized do
-      get 'open_letter', to: 'static_pages#open_letter', as: 'open_letter'
-      get 'biel', to: 'static_pages#biel', as: 'biel'
-      get 'preles', to: 'static_pages#preles', as: 'preles'
-      get 'asyl', to: 'static_pages#asyl', as: 'asyl'
-      get 'donate', to: 'static_pages#donate', as: 'donate'
-      get 'archive', to: 'static_pages#archive', as: 'archive'
-      get 'contact', to: 'static_pages#contact', as: 'contact'
-      get 'press_review', to: 'static_pages#press_review', as: 'press_review'
-      get 'about', to: 'static_pages#about', as: 'about'
-      get 'langbruck', to: 'static_pages#langbruck', as: 'langbruck'
-      get 'children_right', to: 'static_pages#children_right', as: 'children_right'
-      get 'pin_board', to: 'signatures#pin_board', as: 'pin_board'
-    end
+
+  end
+
+  localized do
+    get 'open_letter', to: 'static_pages#open_letter', as: 'open_letter'
+    get 'biel', to: 'static_pages#biel', as: 'biel'
+    get 'preles', to: 'static_pages#preles', as: 'preles'
+    get 'asyl', to: 'static_pages#asyl', as: 'asyl'
+    get 'donate', to: 'static_pages#donate', as: 'donate'
+    get 'archive', to: 'static_pages#archive', as: 'archive'
+    get 'contact', to: 'static_pages#contact', as: 'contact'
+    get 'press_review', to: 'static_pages#press_review', as: 'press_review'
+    get 'about', to: 'static_pages#about', as: 'about'
+    get 'langbruck', to: 'static_pages#langbruck', as: 'langbruck'
+    get 'children_right', to: 'static_pages#children_right', as: 'children_right'
+    get 'pin_board', to: 'signatures#pin_board', as: 'pin_board'
   end
 
   post '/signatures/add_signature', to: "signatures#add_signature", as: 'add_signature'
