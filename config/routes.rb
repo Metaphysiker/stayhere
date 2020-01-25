bbcf04b25f151471f422de86b285642061b35bdf
Metaphysiker <s.raess@me.com>
2020-01-25 10:53:14 +0100

    added translation

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,22 @@ Rails.application.routes.draw do
 
   scope "(:locale)", locale: /de|fr/ do
     root 'static_pages#biel'
-  end
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  localized do
-    get 'open_letter', to: 'static_pages#open_letter', as: 'open_letter'
-    get 'biel', to: 'static_pages#biel', as: 'biel'
-    get 'preles', to: 'static_pages#preles', as: 'preles'
-    get 'asyl', to: 'static_pages#asyl', as: 'asyl'
-    get 'donate', to: 'static_pages#donate', as: 'donate'
-    get 'archive', to: 'static_pages#archive', as: 'archive'
-    get 'contact', to: 'static_pages#contact', as: 'contact'
-    get 'press_review', to: 'static_pages#press_review', as: 'press_review'
-    get 'about', to: 'static_pages#about', as: 'about'
-    get 'langbruck', to: 'static_pages#langbruck', as: 'langbruck'
-    get 'children_right', to: 'static_pages#children_right', as: 'children_right'
-    get 'pin_board', to: 'signatures#pin_board', as: 'pin_board'
+    localized do
+      get 'open_letter', to: 'static_pages#open_letter', as: 'open_letter'
+      get 'biel', to: 'static_pages#biel', as: 'biel'
+      get 'preles', to: 'static_pages#preles', as: 'preles'
+      get 'asyl', to: 'static_pages#asyl', as: 'asyl'
+      get 'donate', to: 'static_pages#donate', as: 'donate'
+      get 'archive', to: 'static_pages#archive', as: 'archive'
+      get 'contact', to: 'static_pages#contact', as: 'contact'
+      get 'press_review', to: 'static_pages#press_review', as: 'press_review'
+      get 'about', to: 'static_pages#about', as: 'about'
+      get 'langbruck', to: 'static_pages#langbruck', as: 'langbruck'
+      get 'children_right', to: 'static_pages#children_right', as: 'children_right'
+      get 'pin_board', to: 'signatures#pin_board', as: 'pin_board'
+    end
   end
 
   post '/signatures/add_signature', to: "signatures#add_signature", as: 'add_signature'
